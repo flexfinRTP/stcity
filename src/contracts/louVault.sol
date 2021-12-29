80b6a88d3aa3429937fd946ca257ea210861cc2c
flexfinRTP <gaffney311@outlook.com>
2021-12-29 17:34:06 -0600

    fe init

--- src/contracts/louVault.sol
+++ src/contracts/louVault.sol
@@ -14,6 +14,8 @@ contract louVault {
 
   //add events
   event Deposit(address indexed user, uint etherAmount, uint timeStart);
+  event Withdraw(address indexed user, uint etherAmount, uint depositTime, uint interest);
+
   //pass as constructor argument deployed Token contract
   constructor(Token _token) public {
     //assign token deployed contract to variable
@@ -25,7 +27,7 @@ contract louVault {
     require(isDeposited[msg.sender] == false, 'Error, deposit already active');
     //check if msg.value is >= than 0.01 ETH
     require(msg.value >= 1e16, 'Error, deposit must be >= 0.01 ETH');
-    
+
     etherBalanceOf[msg.sender] = etherBalanceOf[msg.sender] + msg.value;
     depositStart[msg.sender] = depositStart[msg.sender] + block.timestamp;
 
@@ -41,19 +43,33 @@ contract louVault {
 
   function withdraw() public {
     //check if msg.sender deposit status is true
+    require(isDeposited[msg.sender] == true, 'Error, no previous deposit');
     //assign msg.sender ether deposit balance to variable for event
+    uint userBalance = etherBalanceOf[msg.sender]; //for event
 
     //check user's hodl time
-
+    uint depositTime = block.timestamp - depositStart[msg.sender];
     //calc interest per second
     //calc accrued interest
+    //31668017 - interest(10% APY) per second for min. deposit amount (0.01 ETH), cuz:
+    //1e15(10% of 0.01 ETH) / 31577600 (seconds in 365.25 days)
 
-    //send eth to user
-    //send interest in tokens to user
+    //(etherBalanceOf[msg.sender] / 1e16) - calc. how much higher interest will be (based on deposit), e.g.:
+    //for min. deposit (0.01 ETH), (etherBalanceOf[msg.sender] / 1e16) = 1 (the same, 31668017/s)
+    //for deposit 0.02 ETH, (etherBalanceOf[msg.sender] / 1e16) = 2 (doubled, (2*31668017)/s)
+    uint interestPerSecond = 31668017 * (etherBalanceOf[msg.sender] / 1e16);
+    uint interest = interestPerSecond * depositTime;
+
+    //send funds to user
+    msg.sender.transfer(userBalance); //eth back to user
+    token.mint(msg.sender, interest); //interest to user
 
     //reset depositer data
+    depositStart[msg.sender] = 0;
+    etherBalanceOf[msg.sender] = 0;
+    isDeposited[msg.sender] = false;
 
-    //emit event
+    emit Withdraw(msg.sender, userBalance, depositTime, interest);
   }
 
   function borrow() payable public {
